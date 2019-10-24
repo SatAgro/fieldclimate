d8060885528fe6e9ea618d5c6b0d020745a229ec
Marcin Gałązka <marcgala@hotmail.com>
2019-10-24 15:46:48 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='fieldclimate',
-    version='1.0',
+    version='1.1',
     install_requires=required,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     url='https://github.com/SatAgro/fieldclimate',
